c657c26933c64a6516bc16960ba4e7bedc9d0b8c
czhuyu <czhuyu@gmail.com>
2017-12-22 18:44:57 +0800

    基本完成运动逻辑

--- init.go
+++ init.go
@@ -4,25 +4,44 @@ import (
 	"fmt"
 	//"os/exec"
 	"time"
+	"strings"
+	"strconv"
+     //termbox "github.com/nsf/termbox-go"
+	//"github.com/go-xorm/builder"
+	"github.com/nsf/termbox-go"
+	"log"
 )
 
 func main(){
 
-	var Map [20][20]int
+	var gameMap [20][20]int
+	var snake [50]string
+	//var orientation int
 	//cmd := exec.Command("clear")
 	//_ = cmd.Run()
+
+	snake[0] = "0,0"
+	//orientation = 2
 
 	for {
 		for x:= 0;x<20 ;x++  {
 			fmt.Print("一")
 		}
 		fmt.Println()
+		for z:=0; z<50; z++  {
+			if("" != snake[z]){
+				tempCoordinate := strings.Split(snake[z],",")
+				CoordinateX,_ := strconv.Atoi(tempCoordinate[0])
+				CoordinateY,_ := strconv.Atoi(tempCoordinate[1])
+				gameMap[CoordinateX][CoordinateY] = 1
+			}
+		}
 		for x:=0; x<20; x++ {
 			fmt.Print("|")
 			for y:=0; y<20; y++ {
-				if(Map[x][y] == 1){
+				if(gameMap[x][y] == 1){
 					fmt.Print("口")
-				}else if(Map[x][y] == 0){
+				}else if(gameMap[x][y] == 0){
 					fmt.Print("  ")
 				}
 			}
@@ -31,6 +50,82 @@ func main(){
 		for x:= 0;x<20 ;x++  {
 			fmt.Print("一")
 		}
+
+
+
+		key := make(chan string)
+		go func() {
+			err := termbox.Init()
+			if err != nil {
+				panic(err)
+			}
+			defer termbox.Close()
+		Loop:
+			for {
+				switch ev := termbox.PollEvent(); ev.Type {
+				case termbox.EventKey:
+					switch ev.Key {
+					case termbox.KeyArrowUp:
+						key <- "1"
+					case termbox.KeyArrowRight:
+						key <- "2"
+					case termbox.KeyArrowDown:
+						key <- "3"
+					case termbox.KeyArrowLeft:
+						key <- "4"
+					default:
+						key <- "2"
+						break Loop
+					}
+				default:
+					key <- "2"
+					break Loop
+				}
+				log.Println(key)
+			}
+		}()
+
+		orientation := <-key
+
+		log.Println(orientation)
+
+		if(orientation == "1"){
+			//向上移动蛇
+			tempCoordinate := strings.Split(snake[0],",")
+			tempX,_ := strconv.Atoi(tempCoordinate[0])
+			tempY,_ := strconv.Atoi(tempCoordinate[1])
+			snake[0] = strconv.Itoa(tempX-1) + "," + strconv.Itoa(tempY)
+		}
+		if(orientation == "2"){
+			//向右移动蛇
+			tempCoordinate := strings.Split(snake[0],",")
+			tempX,_ := strconv.Atoi(tempCoordinate[0])
+			tempY,_ := strconv.Atoi(tempCoordinate[1])
+			snake[0] = strconv.Itoa(tempX) + "," + strconv.Itoa(tempY + 1)
+		}
+		if(orientation == "3"){
+			//向下移动蛇
+			tempCoordinate := strings.Split(snake[0],",")
+			tempX,_ := strconv.Atoi(tempCoordinate[0])
+			tempY,_ := strconv.Atoi(tempCoordinate[1])
+			snake[0] = strconv.Itoa(tempX+1) + "," + strconv.Itoa(tempY)
+		}
+		if(orientation == "4"){
+			//向左移动蛇
+			tempCoordinate := strings.Split(snake[0],",")
+			tempX,_ := strconv.Atoi(tempCoordinate[0])
+			tempY,_ := strconv.Atoi(tempCoordinate[1])
+			snake[0] = strconv.Itoa(tempX) + "," + strconv.Itoa(tempY - 1)
+		}
+
+
+		for i:=0; i<20; i++ {
+			for j:=0; j<20; j++ {
+				gameMap[i][j] = 0
+			}
+		}
+
+
 		time.Sleep(time.Second)
 		//cmd := exec.Command("cls")
 		//_ = cmd.Run()
--- test.go
+++ test.go
@@ -1,13 +1,20 @@
 package main
 
 import (
-	//"fmt"
-	"os/exec"
+	"fmt"
+	//"os/exec"
 	//"time"
+	"strings"
+	"reflect"
 )
 
 func main(){
-
-	cmd := exec.Command("cls")
-	_ = cmd.Run()
+	var snake [50]string
+	//cmd := exec.Command("clear")
+	//_ = cmd.Run()
+	snake[0] = "0,0"
+	s := strings.Split(snake[0],",")
+	x, y := s[0], s[1]
+	fmt.Println(reflect.TypeOf(x))
+	fmt.Println(reflect.TypeOf(y))
 }
