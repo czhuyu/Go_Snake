ebc756e9dd51e1692f46e97a682460413c160656
czhuyu <czhuyu@gmail.com>
2017-12-25 12:01:19 +0800

    随机生成食物

--- init.go
+++ init.go
@@ -3,139 +3,33 @@ package main
 import (
 	// #include <conio.h>
 	"C"
-
 	"fmt"
-	//"os/exec"
 	"time"
 	"strings"
 	"strconv"
-	//"github.com/nsf/termbox-go"
-	//"github.com/go-xorm/builder"
-	//"github.com/nsf/termbox-go"
-	//"log"
+	"math/rand"
 )
 
 var gameMap [20][20]int
 var snake [50]string
 var orientation = 2
 var movecount = 0
-var HeadCoordinate = "0,5"
-var tailCoordinate = "0,5"
+var headCoordinate = "0,5" //初始蛇头
+var tailCoordinate = "0,5" //初始蛇尾
+var foodCoordinate = "-1,-1"
+var snakeLen = 3
 
 func main(){
-
-
-	//var orientation int
-	//cmd := exec.Command("clear")
-	//_ = cmd.Run()
-
-	snake[0] = HeadCoordinate
-	//orientation = 2
-
+	snake[0] = headCoordinate
 	for {
-		for x:= 0;x<20 ;x++  {
-			fmt.Print("一")
-		}
-		fmt.Println()
-		for z:=0; z<50; z++  {
-			if("" != snake[z]) {
-				tempHeadCoordinate := strings.Split(snake[z], ",")
-				CoordinateX, _ := strconv.Atoi(tempHeadCoordinate[0])
-				CoordinateY, _ := strconv.Atoi(tempHeadCoordinate[1])
-				gameMap[CoordinateX][CoordinateY] = 1
-			}
-		}
-		for x:=0; x<20; x++ {
-			fmt.Print("|")
-			for y:=0; y<20; y++ {
-				if(gameMap[x][y] == 1){
-					fmt.Print("口")
-				}else if(gameMap[x][y] == 0){
-					fmt.Print("  ")
-				}
-			}
-			fmt.Println("|")
-		}
-		for x:= 0;x<20 ;x++  {
-			fmt.Print("一")
-		}
-
-
-		//log.Println(<-key)
-		if(C.kbhit() == 1){
-			orientation = move(orientation)
-		}
-
-		if(orientation == 1){
-			//向上移动蛇
-			tempHeadCoordinate := strings.Split(snake[0],",")
-			tempHeadX,_ := strconv.Atoi(tempHeadCoordinate[0])
-			tempHeadY,_ := strconv.Atoi(tempHeadCoordinate[1])
-			snake[0] = strconv.Itoa(tempHeadX-1) + "," + strconv.Itoa(tempHeadY)
-			HeadCoordinate = snake[0]
-			if(movecount >= 1){
-				snake[3] = tailCoordinate
-			}
-			snake[1] = tempHeadCoordinate[0] + ","+ tempHeadCoordinate[1]
-			tailCoordinate = snake[1]
-		}else if(orientation == 2){
-			//向右移动蛇
-			tempHeadCoordinate := strings.Split(snake[0],",")
-			tempHeadX,_ := strconv.Atoi(tempHeadCoordinate[0])
-			tempHeadY,_ := strconv.Atoi(tempHeadCoordinate[1])
-			snake[0] = strconv.Itoa(tempHeadX) + "," + strconv.Itoa(tempHeadY + 1)
-			HeadCoordinate = snake[0]
-			if(movecount >= 1){
-				snake[3] = tailCoordinate
-			}
-			snake[1] = tempHeadCoordinate[0] + ","+ tempHeadCoordinate[1]
-			tailCoordinate = snake[1]
-		}else if(orientation ==3){
-			//向下移动蛇
-			tempHeadCoordinate := strings.Split(snake[0],",")
-			tempHeadX,_ := strconv.Atoi(tempHeadCoordinate[0])
-			tempHeadY,_ := strconv.Atoi(tempHeadCoordinate[1])
-			snake[0] = strconv.Itoa(tempHeadX+1) + "," + strconv.Itoa(tempHeadY)
-			HeadCoordinate = snake[0]
-			if(movecount >= 1){
-				snake[3] = tailCoordinate
-			}
-			snake[1] = tempHeadCoordinate[0] + ","+ tempHeadCoordinate[1]
-			tailCoordinate = snake[1]
-		}else{
-			//向左移动蛇
-			tempHeadCoordinate := strings.Split(snake[0],",")
-			tempHeadX,_ := strconv.Atoi(tempHeadCoordinate[0])
-			tempHeadY,_ := strconv.Atoi(tempHeadCoordinate[1])
-			snake[0] = strconv.Itoa(tempHeadX) + "," + strconv.Itoa(tempHeadY - 1)
-			HeadCoordinate = snake[0]
-			if(movecount >= 1){
-				snake[3] = tailCoordinate
-			}
-			snake[1] = tempHeadCoordinate[0] + ","+ tempHeadCoordinate[1]
-			tailCoordinate = snake[1]
-		}
-
+		drawMap()
+		moveSnake()
 		movecount++
-
-
-		for i:=0; i<20; i++ {
-			for j:=0; j<20; j++ {
-				gameMap[i][j] = 0
-			}
-		}
-
-
-		time.Sleep(time.Second)
-		//  cmd := exec.Command("cls")
-		//_ = cmd.Run()
-		for x:=1; x<20; x++{
-			fmt.Println()
-		}
+		resetMap()
 	}
 }
 
-func move(before int) int {
+func changeOrientation(before int) int {
 	key := C.getch()
 	if(key == 119 && before != 3){
 		return 1
@@ -150,12 +44,145 @@ func move(before int) int {
 	}
 }
 
-func getSnakeLen() int {
-	i := 0
+func moveSnake(){
+	//非阻塞获取键盘输入
+	if(C.kbhit() == 1){
+		orientation = changeOrientation(orientation)
+	}
+
+	if(orientation == 1){
+		//向上移动蛇
+		tempheadCoordinate := strings.Split(snake[0],",")
+		tempHeadX,_ := strconv.Atoi(tempheadCoordinate[0])
+		snake[0] = strconv.Itoa(tempHeadX-1) + "," + tempheadCoordinate[1]
+		headCoordinate = snake[0]
+		if(movecount >= 1){
+			snake[2] = tailCoordinate
+		}
+		snake[1] = tempheadCoordinate[0] + ","+ tempheadCoordinate[1]
+		tailCoordinate = snake[1]
+		isGetFood()
+	}else if(orientation == 2){
+		//向右移动蛇
+		tempheadCoordinate := strings.Split(snake[0],",")
+		tempHeadY,_ := strconv.Atoi(tempheadCoordinate[1])
+		snake[0] = tempheadCoordinate[0] + "," + strconv.Itoa(tempHeadY + 1)
+		headCoordinate = snake[0]
+		if(movecount >= 1){
+			snake[2] = tailCoordinate
+		}
+		snake[1] = tempheadCoordinate[0] + ","+ tempheadCoordinate[1]
+		tailCoordinate = snake[1]
+		isGetFood()
+	}else if(orientation ==3){
+		//向下移动蛇
+		tempheadCoordinate := strings.Split(snake[0],",")
+		tempHeadX,_ := strconv.Atoi(tempheadCoordinate[0])
+		snake[0] = strconv.Itoa(tempHeadX+1) + "," + tempheadCoordinate[1]
+		headCoordinate = snake[0]
+		if(movecount >= 1){
+			snake[2] = tailCoordinate
+		}
+		snake[1] = tempheadCoordinate[0] + ","+ tempheadCoordinate[1]
+		tailCoordinate = snake[1]
+		isGetFood()
+	}else{
+		//向左移动蛇
+		tempheadCoordinate := strings.Split(snake[0],",")
+		tempHeadY,_ := strconv.Atoi(tempheadCoordinate[1])
+		snake[0] = tempheadCoordinate[0] + "," + strconv.Itoa(tempHeadY - 1)
+		headCoordinate = snake[0]
+		if(movecount >= 1){
+			snake[2] = tailCoordinate
+		}
+		snake[1] = tempheadCoordinate[0] + ","+ tempheadCoordinate[1]
+		tailCoordinate = snake[1]
+		isGetFood()
+	}
+}
+
+func drawMap(){
+	if(movecount >= 2 && foodCoordinate == "-1,-1"){
+		drawFood()
+	}
+	for x:= 0;x<20 ;x++  {
+		fmt.Print("一")
+	}
+	fmt.Println()
 	for z:=0; z<50; z++  {
 		if("" != snake[z]) {
-			i++
+			tempheadCoordinate := strings.Split(snake[z], ",")
+			CoordinateX, _ := strconv.Atoi(tempheadCoordinate[0])
+			CoordinateY, _ := strconv.Atoi(tempheadCoordinate[1])
+			gameMap[CoordinateX][CoordinateY] = 1
+		}
+		if(foodCoordinate != "-1,-1"){
+			tempheadCoordinate := strings.Split(foodCoordinate, ",")
+			CoordinateX, _ := strconv.Atoi(tempheadCoordinate[0])
+			CoordinateY, _ := strconv.Atoi(tempheadCoordinate[1])
+			gameMap[CoordinateX][CoordinateY] = 2
 		}
 	}
-	return i
+	for x:=0; x<20; x++ {
+		fmt.Print("|")
+		for y:=0; y<20; y++ {
+			if(gameMap[x][y] == 1){
+				fmt.Print("口")
+			}else if(gameMap[x][y] == 2){
+				fmt.Print("△")
+			}else{
+				fmt.Print("  ")
+			}
+		}
+		fmt.Println("|")
+	}
+	for x:= 0;x<20 ;x++  {
+		fmt.Print("一")
+	}
+}
+
+func resetMap(){
+	for i:=0; i<20; i++ {
+		for j:=0; j<20; j++ {
+			gameMap[i][j] = 0
+		}
+	}
+	time.Sleep(time.Second)
+	for x:=1; x<20; x++{
+		fmt.Println()
+	}
+}
+
+func drawFood(){
+	for {
+		randX := generateRandnum()
+		randY := generateRandnum()
+		if(gameMap[randX][randY] == 0){
+			gameMap[randX][randY] = 2
+			foodCoordinate = strconv.Itoa(randX) + "," + strconv.Itoa(randY)
+			return
+		}else{
+			continue
+		}
+	}
+
+}
+func generateRandnum() int {
+	rand.Seed(time.Now().Unix())
+	randNum := rand.Intn(19)
+	return randNum
+}
+
+func isGetFood(){
+	if(headCoordinate == foodCoordinate){
+		//吃到了食物
+		drawFood()
+		//蛇身增长
+	}else{
+		return
+	}
+}
+
+func addSnakeLen(){
+	snake[snakeLen] = tailCoordinate
 }
--- test.go
+++ test.go
@@ -1,17 +1,44 @@
 package main
 
+import "fmt"
 import (
-	// #include <conio.h>
-	"C"
-	"fmt"
+	"time"
+	"bufio"
+	"os"
 )
 
-func Move() {
-	key := C.getch()
-	fmt.Println(key)
-}
+func main(){
+	ch := make(chan string)
+	go func(ch chan string) {
+		/* Uncomment this block to actually read from stdin
 
-func main() {
-	Move()
-}
+		*/
+		reader := bufio.NewReader(os.Stdin)
+		for {
+			s, err := reader.ReadString('\n')
+			if err != nil { // Maybe log non io.EOF errors, if you want
+				close(ch)
+				return
+			}
+			ch <- s
+		}
+		// Simulating stdin
+		ch <- "A line of text"
+		close(ch)
+	}(ch)
 
+stdinloop:
+	for {
+		select {
+		case stdin, ok := <-ch:
+			if !ok {
+				break stdinloop
+			} else {
+				fmt.Println("Read input from stdin:", stdin)
+			}
+		case <-time.After(1 * time.Second):
+			// Do something when there is nothing read from stdin
+		}
+	}
+	fmt.Println("Done, stdin must be closed")
+}
